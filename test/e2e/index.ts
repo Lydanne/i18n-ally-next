c1c9f48fc456f3fea075f4dda6482fc2d7045c8a
Lydanne <1829913225@qq.com>
2026-02-20 18:18:07 +0800

    test: setup fixtures folder for e2e tests and add issue-3 test

--- test/e2e/index.ts
+++ test/e2e/index.ts
@@ -13,16 +13,22 @@ async function main() {
 
   // Frameworks
   const testFrameworksDir = join(root, 'test/e2e-out/frameworks')
+  const testFixturesDir = join(root, 'test/e2e-out/fixtures')
   const fixtureTempPath = join(root, 'test/e2e-fixtures-temp')
 
   if (fs.existsSync(fixtureTempPath))
     await fs.remove(fixtureTempPath)
 
   const frameworks = args.length
-    ? args
+    ? args.filter(a => !a.startsWith('issue-'))
     : await fg('*', { onlyDirectories: true, cwd: testFrameworksDir })
 
+  const fixtures = args.length
+    ? args.filter(a => a.startsWith('issue-'))
+    : await fg('*', { onlyDirectories: true, cwd: testFixturesDir })
+
   try {
+    // Run framework tests
     for (const framework of frameworks) {
       console.log(`\n\n${chalk.blue('Start E2E testing for framework')} ${chalk.magenta(framework)} ${chalk.blue('...\n')}`)
       const extensionTestsPath = join(testFrameworksDir, framework, 'index')
@@ -39,6 +45,25 @@ async function main() {
       })
 
       console.log(chalk.green(`E2E tests for framework ${framework} finished.\n`))
+    }
+
+    // Run fixture tests
+    for (const fixture of fixtures) {
+      console.log(`\n\n${chalk.blue('Start E2E testing for fixture')} ${chalk.magenta(fixture)} ${chalk.blue('...\n')}`)
+      const extensionTestsPath = join(testFixturesDir, fixture, 'index')
+      const fixtureSourcePath = join(root, 'examples/by-fixtures', fixture)
+      const fixtureTargetPath = join(fixtureTempPath, fixture)
+
+      await fs.copy(fixtureSourcePath, fixtureTargetPath)
+
+      await runTests({
+        extensionDevelopmentPath,
+        extensionTestsPath,
+        version: '1.106.0',
+        launchArgs: [fixtureTargetPath, '--disable-extensions'],
+      })
+
+      console.log(chalk.green(`E2E tests for fixture ${fixture} finished.\n`))
     }
 
     process.exit(0)
